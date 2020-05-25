4949f5f3b37f95f313856e0ecf5c26b3a6a1bc33
Denis <denis.selditsin@ya.ru>
2020-05-25 14:51:54 +0300

    Сделал лобстератор

---
+++ main.py
@@ -0,0 +1,8 @@
+from Lobsterator import Lobsterator
+
+if __name__ == '__main__':
+    img = r"img\cat.png"
+    text = "мемный кекст"
+
+    lobster = Lobsterator(img, text)
+    lobster.lobsterate()
