51d8dd038df6b033414dbf8ca398ea6e6caffdbe
Kengston <60067857+Kengston@users.noreply.github.com>
2020-05-28 23:40:27 +0700

    Еще один уебанский коммит
    Ребята, я очень сильно извиняюсь за уебанские коммиты, мне лень было разбираться с контролем версий в VS, добавил возможность парсить датасет из 15-ти фотографий, выгрузку инфы о них в отдельный файл.
    Кто хочет поработать с данным кодом, очень рекомендую ознакомиться и скачать:
    ml5.js - https://learn.ml5js.org/docs/#/
    p5.js - p5.js download mac

--- ml5/sketch.js
+++ ml5/sketch.js
@@ -1,17 +1,74 @@
+// Выгрузка ImageNet
 const classifier = new ml5.ImageClassifier('MobileNet');
 
 let img;
+let currentIndex = 0;
+let allImages = [];
+let display = true;
+let displayTime = 750;
+let predictions = [];
+
+function appendImages() {
+  for (i = 0; i < data.images.length; i++) {
+    imgPath = data.images[i];
+    allImages.push(getImagePath(imgPath));
+  }
+}
+
+function preload() {
+  data = loadJSON('/assets/data.json');
+}
+
+function getImagePath(imgPath) {
+  fullPath = 'images/dataset/';
+  fullPath = fullPath + imgPath;
+  return fullPath
+}
+
+function drawNextImage() {
+  img.attribute('src', allImages[currentIndex], imageReady);
+}
 
 function setup() {
   noCanvas();
-  // Загрузка фотки
-  img = createImg('images/doggo.jpg', imageReady);
+  appendImages();
+  img = createImg(allImages[0], imageReady);
 }
 
+
+//Дает прогноз когда картинка загружена
 function imageReady() {
   classifier.predict(img.elt, 10, gotResult);
 }
 
+function savePredictions() {
+  predictionsJSON = {
+    "predictions": predictions
+  }
+  saveJSON(predictionsJSON, 'predictions.json');
+}
+
+function removeImage() {
+  currentIndex++;
+  if (currentIndex <= allImages.length - 1) {
+    drawNextImage();
+  } else {
+    savePredictions();
+  }
+}
+
+// Когда получаем результат
 function gotResult(results) {
-  select('#result').html(results[0].label);
+  information = {
+    "name": allImages[currentIndex],
+    "result": results,
+  }
+  predictions.push(information);
+
+  if (display) {
+    select('#result').html(results[0].label);
+    setTimeout(removeImage, displayTime);
+  } else {
+    removeImage();
+  }
 }
